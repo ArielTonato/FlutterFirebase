2d399ada674638fb22064008d54fa25dd5ef2a18
Ariel <alexarieltonato@hotmail.com>
2023-06-07 14:22:04 -0500

    Aprendiendo flutter con firebase

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_crud/services/firebase_service.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: FutureBuilder(
+        future: getPeople(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                return Text(snapshot.data?[index]['name']);
+              },
+            );
+          }else{
+            return const Center(
+              child: CircularProgressIndicator(),
+            ) ;
+          }
+        },
+      ),
+    );
+  }
+}
